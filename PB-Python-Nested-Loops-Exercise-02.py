1e8aae427eb265db21eb6794845c587131cb4e6b
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2021-05-20 10:35:47 +0300

    Fixed a bug on ex.2

--- PB-Python-Nested-Loops-Exercise-02.py
+++ PB-Python-Nested-Loops-Exercise-02.py
@@ -1,10 +1,10 @@
 a = int(input())
 b = int(input())
-for i in range(a + 1, b):
-    sume = 0
-    sumo = 0
-    for index, c in enumerate(str(i)):
+for i in range(a, b + 1):
+    sumo, sume = (0, 0)
+    for index, digit in enumerate(str(i)):
         if index % 2 == 0:
-            sume += int(c)
-        else: sumo += int(c)
+            sume += int(digit)
+        else:
+            sumo += int(digit)
     if sumo == sume: print(i, end=" ")
